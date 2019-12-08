7f758d4b90fe812ba1653033a37274e00849e716
zhangShuang <1969533391@qq.com>
2019-12-08 11:37:22 +0800

    complete the most js

--- tabao/index.js
+++ tabao/index.js
@@ -7,13 +7,13 @@ let article = `
             <a href="">
                 <img
                     src="images/shop.png"
-                    class="logo"
+                    class="logo_p"
                     alt="店铺"
                 />
                 <p>tiger worksll</p>
                 <img
                     src="images/right.png"
-                    class="logo"
+                    class="logo_p"
                     alt="详情"
                 />
             </a>
@@ -88,16 +88,138 @@ let article = `
         </div>
     </div>
 `;
-function add_good_node(html, num){
-    while(num--){
-        let content = document.getElementsByClassName("shangping")[0];
-        let article_node = document.createElement("article");
-        article_node.className = "good_ifo";
+function add_good_node(html, num) {
+    while (num--) {
+        let content = document.getElementsByClassName('shangping')[0];
+        let article_node = document.createElement('article');
+        article_node.className = 'good_ifo';
         article_node.innerHTML = html;
-        content.appendChild(article_node)
+        content.appendChild(article_node);
     }
 }
-window.onload = function(){
-    this.add_good_node(article,5);
+function add_good_num() {
+    let goods = document.getElementsByClassName('num');
+    let length = goods.length;
+    for (let i = 0; i < length; i++) {
+        let move_good = goods[i].getElementsByTagName('img')[0];
+        let add_good = goods[i].getElementsByTagName('img')[1];
+        let good_num = goods[i].getElementsByTagName('div')[0];
+        move_good.addEventListener('click', function() {
+            if (good_num.innerHTML == 1) {
+            } else {
+                good_num.innerHTML--;
+            }
+        });
+        add_good.addEventListener('click', function() {
+            good_num.innerHTML++;
+        });
+    }
 }
+function xuanze() {
+    let xuanzes = document.getElementsByClassName('good_ifo_main');
+    let length = xuanzes.length;
 
+    for (let i = 0; i < length; i++) {
+        let round = xuanzes[i]
+            .getElementsByClassName('xuanze')[0]
+            .getElementsByTagName('img')[0];
+        round.addEventListener('click', function() {
+            let attr = round.getAttribute('src');
+            attr =
+                attr === 'images/round_check.png'
+                    ? 'images/round.png'
+                    : 'images/round_check.png';
+            round.setAttribute('src', attr);
+        });
+    }
+}
+function xunaze_dianpu() {
+    let goods = document.getElementsByClassName('good_ifo');
+    let length = goods.length;
+    for (let i = 0; i < length; i++) {
+        let dianpu_xuanze = goods[i]
+            .getElementsByTagName('header')[0]
+            .getElementsByClassName('xuanze')[0]
+            .getElementsByTagName('img')[0];
+        let goods_xuanzes = goods[i]
+            .getElementsByClassName('good_ifo_main')[0]
+            .getElementsByClassName('logo');
+        dianpu_xuanze.addEventListener('click', function() {
+            let src = dianpu_xuanze.getAttribute('src');
+            if (src === 'images/round_check.png') {
+                dianpu_xuanze.setAttribute('src', 'images/round.png');
+                for (let goods_src of goods_xuanzes) {
+                    goods_src.setAttribute('src', 'images/round.png');
+                }
+            } else {
+                dianpu_xuanze.setAttribute('src', 'images/round_check.png');
+                for (let goods_src of goods_xuanzes) {
+                    goods_src.setAttribute('src', 'images/round_check.png');
+                }
+            }
+        });
+    }
+}
+function get_all() {
+    let all_price = 0;
+    let goods = document.getElementsByClassName('good_ifo_main');
+    let heji = document.getElementById('heji').getElementsByTagName('p')[0];
+
+    for (let good of goods) {
+        let isxunaze = good.getElementsByClassName('logo')[0];
+        isxunaze = isxunaze.getAttribute('src') === 'images/round_check.png';
+        let good_price = good.getElementsByClassName('jiage')[0];
+        good_price = good_price.innerHTML.match(/\d+(.\d+)?/g)[0];
+        good_price = parseFloat(good_price);
+        let num = good.getElementsByClassName('num_of_goods')[0];
+        num = parseInt(num.innerHTML);
+        if (isxunaze) {
+            all_price += num * good_price;
+        }
+    }
+    all_price = all_price.toFixed(2);
+    heji.innerHTML = '￥' + all_price.toString();
+}
+function jianting() {
+    let logos = document.getElementsByClassName('logo');
+    for (let logo of logos) {
+        logo.addEventListener('click', this.get_all);
+    }
+    let changes = document.getElementsByClassName('num');
+    for (let change of changes) {
+        change_img = change.getElementsByTagName('img');
+        change_img[0].addEventListener('click', this.get_all);
+        change_img[1].addEventListener('click', this.get_all);
+    }
+}
+function quanxuan_fun() {
+    let that = this;
+    let quanxuan = document
+        .getElementById('quanxuan')
+        .getElementsByTagName('img')[0];
+    quanxuan.addEventListener('click', function() {
+        let attr = quanxuan.getAttribute('src');
+        let logos = document.getElementsByClassName('logo');
+        if (attr === 'images/round.png') {
+            quanxuan.setAttribute('src', 'images/round_check.png');
+            for (let logo of logos) {
+                logo.setAttribute('src', 'images/round_check.png');
+            }
+        } else {
+            quanxuan.setAttribute('src', 'images/round.png');
+            for (let logo of logos) {
+                logo.setAttribute('src', 'images/round.png');
+            }
+        }
+        that.get_all();
+    });
+}
+window.onload = function() {
+    this.add_good_node(article, 5);
+    this.add_good_num();
+    this.xuanze();
+    this.xunaze_dianpu();
+    this.jianting();
+    this.get_all();
+    this.quanxuan_fun();
+};
